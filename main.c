98d1ded2e2dfee44c656c827d4d6ab480f0d264e
zwx19961130 <zwx19961130@icloud.com>
2017-02-05 18:05:31 +0800

    22

---
+++ main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include<GL/glut.h>
+#include<FreeImage.h>
+
+void init() {
+	LoadGLTextures();
+	glEnable(GL_TEXTURE_2D);
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	
+}
+
+void changeParam() {
+	rtri += 0.05f;
+	glutPostRedisplay();
+}
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(400, 400);
+	glutCreateWindow("EarthDemo");
+	init();
+	return 0;
+}
